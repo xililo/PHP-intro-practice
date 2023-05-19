9f8611b29ea7d7dcd20623bc7ef1dd85d3221966
xililo <sililosililo82@gmail.com>
2023-05-19 13:20:18 +0200

    initial

---
+++ process.php
@@ -0,0 +1,13 @@
+<?php
+
+
+echo 'here we recieve data';
+echo '<br>';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+echo 'Your name is ' . $name;
+echo '<br>';
+echo 'Your Email is ' . $email;
+echo '<br>';
+echo 'Your phone number is ' . $phone;
